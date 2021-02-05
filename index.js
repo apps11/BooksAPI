513a959a778e4a127381aedc3aeb523469fe6de9
Apps <arpitsavaliya1234@gmail.com>
2021-02-06 00:33:34 +0530

    complete

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const route = require('./route');
+const bodyparser = require('body-parser');
+
+mongoose.connect("mongodb://localhost:27017/db",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+    ()=>{
+        const app = express();
+        app.use(bodyparser.urlencoded({extended:true}));
+        app.use(express.json());
+        app.use("/api",route);
+        app.listen(process.env.PORT || 1111,()=>{
+            console.log("server started....");
+        });
+    }
+);
+
+//mongodb+srv://Apps:Anivlak<11@cluster0.hlbvp.mongodb.net/db?retryWrites=true&w=majority
+
+//apps
