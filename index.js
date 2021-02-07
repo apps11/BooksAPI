f2d0d154d1438baf5608b3416f4475bc1028a976
Apps <arpitsavaliya1234@gmail.com>
2021-02-07 22:28:16 +0530

    working code

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const bodyparser = require('body-parser')
+const bodyparser = require('body-parser');
 const mongoose = require('mongoose');
-const route = require('./route')
+const route = require('./route');
 
-mongoose.connect('mongodb+srv://User:2ZSOtQST24RTD61T@cluster0.hlbvp.mongodb.net/db?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true}).then(
+mongoose.connect("mongodb+srv://User:2ZSOtQST24RTD61T@cluster0.hlbvp.mongodb.net/db?retryWrites=true&w=majority", {useNewUrlParser: true, useUnifiedTopology: true}).then(
    ()=>{
     const app = express();
     app.use(bodyparser.urlencoded({extended:true}));
@@ -11,7 +11,6 @@ mongoose.connect('mongodb+srv://User:2ZSOtQST24RTD61T@cluster0.hlbvp.mongodb.net
     app.use("/api",route);
     app.listen(process.env.PORT || 3000,()=>{
         console.log("server Started...");
-    })
+    });
    }    
-)
-
+);
